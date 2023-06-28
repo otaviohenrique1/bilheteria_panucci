0a603ab1f1fe264b8c50082255f2df6a58a7b81e
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-27 22:54:33 -0300

    commit 4

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -47,24 +47,43 @@ class _HomeState extends State<Home> {
                       ),
                     );
                   } else if (state is HomeSuccess) {
-                    return SliverGrid.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 16,
-                        mainAxisExtent: 240,
-                      ),
-                      itemBuilder: (context, index) {
-                        return MovieCard(movie: state.movies[index]);
-                      },
-                      itemCount: state.movies.length,
-                    );
+                    if (state.movies.isEmpty) {
+                      return const SliverFillRemaining(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.not_interested,
+                              size: 30.0,
+                            ),
+                            SizedBox(height: 16.00),
+                            Text("Não existem filmes com este gênero!"),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return SliverGrid.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 16,
+                          mainAxisExtent: 240,
+                        ),
+                        itemBuilder: (context, index) {
+                          return MovieCard(movie: state.movies[index]);
+                        },
+                        itemCount: state.movies.length,
+                      );
+                    }
                   } else if (state is HomeError) {
                     return SliverFillRemaining(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Icon(Icons.not_interested),
+                          const Icon(
+                            Icons.not_interested,
+                            size: 30.0,
+                          ),
                           const SizedBox(height: 16.00),
                           Text(state.error),
                         ],
